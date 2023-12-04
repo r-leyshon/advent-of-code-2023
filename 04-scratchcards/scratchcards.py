cb73fb01453a97318d17693ad7a596b954dcf7e0
r-leyshon <richard.leyshon@ons.gov.uk>
2023-12-04 09:16:06 +0000

    feat: Solution day 4 part 2

--- 04-scratchcards/scratchcards.py
+++ 04-scratchcards/scratchcards.py
@@ -3,7 +3,7 @@ from pyprojroot import here
 with open(here("04-scratchcards/input.txt"), "r") as f:
     txt = f.read()
     f.close()
-    
+
 txt = txt.splitlines()
 # sort out the scratchcards:
 scratchcards = {}
@@ -12,6 +12,7 @@ for card in txt:
     card, winners = winners.split(": ")
     winners = winners.split(" ")
     nums = nums.split(" ")
+    card = int(card.split(" ")[-1])
     # converts to ints
     winners = [int(n) for n in winners if n != ""]
     nums = [int(n) for n in nums if n != ""]
@@ -42,6 +43,28 @@ for r in win_record:
     
 print(f"The answer to part 1 is: {sum(points_record)}")
 # part 2
-
 n_cards = len(txt)
+# get the duplicate cards
+card_pile = list(zip(scratchcards.keys(), win_record)) # use this to keep track
+# of processed cards card pile tracks number of cards. key is card num, value
+# is a list where first int is number of winning numbers, second int is number
+# of cards of this type
+card_pile = { k: [v, 1] for k,v in zip(scratchcards.keys(), win_record)}
 
+for k, v in card_pile.items():
+    # condition that allows addition of more cards while card count is > 0:
+    while v[-1] > 0:
+        n_wins = v[0]
+        prize_cards = []
+        for r in range(0, n_wins):
+            prize_cards.append(k + 1 + r)
+        
+        # remove the redeemed card
+        v[-1] -= 1
+        # add the prize cards
+        for prize in prize_cards:
+            card_pile[prize][-1] += 1
+            # increase the number of cards total
+            n_cards += 1
+
+print(f"The answer to part 2 is: {n_cards}")
