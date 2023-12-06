e7047de57aba2f20e8c2dfc7807c3f63db7638bf
r-leyshon <richard.leyshon@ons.gov.uk>
2023-12-06 07:41:55 +0000

    refactor: Func that extracts win option

--- 06-wait-for-it/solution.py
+++ 06-wait-for-it/solution.py
@@ -10,19 +10,26 @@ times = [int(t) for t in times]
 distances = [d for d in txt[-1].split(" ") if d != ""][1:]
 distances = [int(d) for d in distances]
 
-races = zip(times, distances)
-n_wins = []
-for race in races:
-    speed = 0
-    race_wins = 0
-    # increment speed & check if this will exceed distance record
-    for s in range(0, (race[0])):
-        speed +=1
-        remaining_time = race[0] - speed
-        if speed * remaining_time > race[-1]:
-            race_wins += 1
-        else:
-            pass
-    n_wins.append(race_wins)
 
+def get_win_options(times:list, distances:list) -> list:
+    races = zip(times, distances)
+    n_wins = []
+    for race in races:
+        speed = 0
+        race_wins = 0
+        # increment speed & check if this will exceed distance record
+        for s in range(0, (race[0])):
+            speed +=1
+            remaining_time = race[0] - speed
+            if speed * remaining_time > race[-1]:
+                race_wins += 1
+            else:
+                pass
+        n_wins.append(race_wins)
+    
+    return n_wins
+
+
+n_wins = get_win_options(times=times, distances=distances)
 print(f"The answer to part one is {prod(n_wins)}")
+# part 2 - collapse the poorly kerned(!) races
