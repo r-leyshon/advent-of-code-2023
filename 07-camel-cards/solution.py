154cfeb26524fa3ea818b5385ca4b9d5439aac0d
r-leyshon <richard.leyshon@ons.gov.uk>
2023-12-08 06:47:32 +0000

    WIP: Solution day 7

--- 07-camel-cards/solution.py
+++ 07-camel-cards/solution.py
@@ -1,4 +1,5 @@
 from pyprojroot import here
+import pandas as pd
 
 def read_and_process_cards(pth):
     all_games = []
@@ -12,23 +13,23 @@ def read_and_process_cards(pth):
         all_games.append([hand, bid])
     return all_games
 example_pth = here("07-camel-cards/example.txt")
-example_games = read_and_process_cards(example_pth)
-
-
-HAND_TYPE_MAP = {
-    # maps outcome of hand type logic to numerical order
-    1: 7,
-    2: 6,
-    3: 5,
-    4: 4,
-    5: 3,
-    6: 2,
-    7: 1,
-}
+# example_games = read_and_process_cards(example_pth)
+input_pth = here("07-camel-cards/input.txt")
+input_games = read_and_process_cards(input_pth)
 
 
 def get_hand_worth(games: list):
     """Get the worth of each hand based on unique characters."""
+    HAND_TYPE_MAP = {
+        # maps outcome of hand type logic to numerical order
+        1: 7,
+        2: 6,
+        3: 5,
+        4: 4,
+        5: 3,
+        6: 2,
+        7: 1,
+    }
     worth = []
     def _get_character_num_appearances(char, _str):
         return len([i for i in _str if i == char])
@@ -73,24 +74,62 @@ def get_hand_worth(games: list):
 
         # append the hand's worth to the game_list
         worth.append(hand_worth)
+
     return worth
 
 
-hand_worths = get_hand_worth(example_games)
-CARD_VALUE_MAP = {
-    "A": 14, 
-    "K": 13, 
-    "Q": 12, 
-    "J": 11, 
-    "T": 10, 
-    "9": 9, 
-    "8": 8, 
-    "7": 7, 
-    "6": 6, 
-    "5": 5, 
-    "4": 4, 
-    "3": 3,
-    }
+# hand_worths = get_hand_worth(example_games)
+hand_worths = get_hand_worth(input_games)
 
-for games in zip(example_games, hand_worths):
-    print(games)
+
+def rank_hands_into_df(some_games:list, their_worth:list):
+    """Creates a df of all hands ranked by their worth, calculates winnings."""
+    CARD_VALUE_MAP = {
+        "A": 14, 
+        "K": 13, 
+        "Q": 12, 
+        "J": 11, 
+        "T": 10, 
+        "9": 9, 
+        "8": 8, 
+        "7": 7, 
+        "6": 6, 
+        "5": 5, 
+        "4": 4, 
+        "3": 3,
+        "2": 2
+        }
+    game_df = pd.DataFrame()
+    card_sorts_colnms = [f"sort_{i}" for i in range(2, len(some_games[0][0]) + 1)]
+    for i, games in enumerate(zip(some_games, their_worth)):
+        hand_dict = {}
+        # print(games)
+        cards = [*games[0][0]]
+        card_vals = [CARD_VALUE_MAP[c] for c in cards]
+        # print(card_vals)
+        hand_dict["hand"] = games[0][0]
+        hand_dict["bid"] = games[0][1]
+        hand_dict["sort_1"] = games[-1]
+        for i, colnm in enumerate(card_sorts_colnms):
+            hand_dict[colnm] = card_vals[i]
+        # print(hand_dict)
+        row = pd.DataFrame(hand_dict, index=[i])
+        # print(row)
+        game_df = pd.concat([game_df, row])
+
+    # sort by rank columns only
+    game_df = game_df.sort_values(by=[c for c in game_df.columns if c.startswith("sort_")],axis=0)
+    game_df = game_df.reset_index(drop=True)
+    # include an index column
+    game_df = game_df.reset_index()
+    # ranks should start at 1
+    game_df["index"] = game_df["index"]+1
+    game_df["winnings"] = game_df["index"] * game_df["bid"]
+
+    return game_df
+
+
+# sorted_hand = rank_hands_into_df(example_games, hand_worths) # correct
+sorted_hand = rank_hands_into_df(input_games, hand_worths)
+print(f"The solution to part 1 is {sum(sorted_hand['winnings'])}")
+# 248569442 too low 
